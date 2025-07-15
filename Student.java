8a0f914d46c431b4c6acba9c1b168c71fe6e81f3
TruongPhuc30 <caodangtruongphuc3006@gmail.com>
2025-07-15 11:28:01 +0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -5,48 +5,60 @@ public class Student {
     private String id;
     private  String group;
     private String email;
+    // phương thức khởi tạo không tham số
     public Student(){
         this.name = "Student";
         this.id = "000";
         this.group = "K62CB";
         this.email = "uet@vnu.edu.vn";
     }
+    // phương thức khởi tạo có tham số
     public Student(String name, String id, String email){
         this.name = name;
         this.email = email;
         this.id = id;
         this.group = "K62CB";
     }
+    // phương thức khởi tạo sao chép
     public Student(Student s){
         this.name = s.name;
         this.email = s.email;
         this.id = s.id;
         this.group = s.group;
     }
+    // getter cho thuộc tính name
     public String getName(){
         return name;
     }
+    // setter cho thuộc tính name
     public void setName(String n) {
         name = n;
     }
+    // getter cho thuộc tính id
     public String getId(){
         return id;
     }
+    // setter cho thuộc tính id
     public void setId(String i) {
         id = i;
     }
+    // getter cho thuộc tính group
     public String getGroup(){
         return group;
     }
+    // setter cho thuộc tính group
     public void setGroup(String g) {
         group = g;
     }
+    // getter cho thuộc tính email
     public String getEmail(){
         return email;
     }
+    // setter cho thuộc tính email
     public void setEmail(String e) {
         email = e;
     }
+    // getter cho thuộc tính info
     public String getInfo(){
         return name + " - " + id + " - " + group + " - " + email ;
     }
